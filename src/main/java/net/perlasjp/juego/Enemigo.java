9eea102b32fd89b889bc2fc2869b520c6a4060f0
Genesis_Aguilar <evaaguilar.ea81@gmail.com>
2025-07-30 16:18:19 -0600

    complete clase Enemigo

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -1,5 +1,26 @@
 package net.perlasjp.juego;
 
 public class Enemigo {
+        private String nombre;
+    private int salud;
+    private int fuerza;
+
+    public int atacar() {
+        return 0;
+
+    }
+
+    public void recibirDanio(int danio) {
+
+    }
+
+    public boolean estaVivo() {
+
+        return false;
+
+    }
+
+}
+
 
 }
