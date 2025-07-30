e18b984b4a61c400623bae176b0c5521275891aa
helen mejia <mejiahelen271@gmail.com>
2025-07-30 10:01:43 -0600

    complete clase Juego

--- src/main/java/net/perlasjp/juego/Juego.java
+++ src/main/java/net/perlasjp/juego/Juego.java
@@ -1,5 +1,12 @@
 package net.perlasjp.juego;
 
 public class Juego {
+    private Jugador jugador;
+    private Enemigo enemigo;
+    private Combate combate;
+
+    public void iniciar(){
+        
+    }
     
 }
