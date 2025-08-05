6317052ac826f4980e4ebbc3a7c7441229c63607
orlin-perez <Orlinperez218@gmail.com>
2025-08-05 08:17:13 -0600

    Complete clase Jugador

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -1,12 +1,12 @@
 package net.perlasjp.juego;
 
 public class Jugador {
-    private String nombre;
+private String nombre;
     private int salud;
     private int fuerza;
 
     public int atacar(){
-
+       return 0;
     }
 public void recibir(){
 
@@ -17,7 +17,11 @@ public void recibirDanio(danio: int){
 }
 
 public boolean estaVivo(){
+    return false;
     
 }
 
 }
+
+
+
