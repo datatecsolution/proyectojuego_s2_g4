0e53d438cd8f6882dccc230c9dc755aa6b5a4368
helen mejia <mejiahelen271@gmail.com>
2025-08-11 09:58:13 -0600

    agregue metodos y funciones a clase juego

--- src/main/java/net/perlasjp/juego/Juego.java
+++ src/main/java/net/perlasjp/juego/Juego.java
@@ -1,11 +1,60 @@
 package net.perlasjp.juego;
 
+import java.util.Scanner;
+
 public class Juego {
     private Jugador jugador;
     private Enemigo enemigo;
     private Combate combate;
 
     public void iniciar(){
+        Scanner scanner = new Scanner (System.in);
+        combate =new Combate();
+        Historia historia=new Historia();
+
+        System.out.println("¡Bienvenido al juego de aventura!");
+        System.out.print("Ingrese tu nombre: ");
+        String nombre = scanner.nextLine();
+        jugador = new Jugador();
+
+       historia.introduccion();
+       
+       while (true) {
+        System.out.println("\n¿Que deseas hacer?");
+        System.out.println("1. Explorar");
+        System.out.println("2. Ver estado");
+        System.out.println("3. Salir");
+
+
+        String opcion = scanner.nextLine();
+
+        switch (opcion) {
+            case "1":
+                Enemigo enemigo = new Enemigo();
+                combate.iniciar(enemigo , jugador);
+                break;
+
+            case "2":
+        
+                        //vamos a agregar otro metodo en jugador
+
+                    break;
+            case "3":
+                System.out.println("¡Gracias por jugar!");
+                return;
+            default:
+              System.out.println("Opcion no valida.");
+                    
+
+
+
+
+
+        }
+       }
+       
+
+        
         
     }
     
