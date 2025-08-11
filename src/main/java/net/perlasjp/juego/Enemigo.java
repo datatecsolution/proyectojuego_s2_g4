d43883393901098e94aae320808f79579b974783
genesis aguilar <evaaguilar.ea81@gmail.com>
2025-08-11 15:24:55 -0600

    Agregue metodos y funciones a clase Enemigo

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -1,22 +1,27 @@
 package net.perlasjp.juego;
 
 public class Enemigo {
-        private String nombre;
+    private String nombre;
     private int salud;
     private int fuerza;
 
     public int atacar() {
-        return 0;
+        return fuerza;
 
     }
 
-    public void recibirDanio(int danio) {
+    public void recibirDanio(int cantidad) {
+        salud = salud - cantidad;
+        if (salud < 0) {
+            salud = 0;
+
+        }
 
     }
 
     public boolean estaVivo() {
 
-        return false;
+        return salud > 0;
 
     }
 
--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -1,27 +1,25 @@
 package net.perlasjp.juego;
 
 public class Jugador {
-private String nombre;
+    private String nombre;
     private int salud;
     private int fuerza;
 
-    public int atacar(){
-       return 0;
+    public int atacar() {
+        return 0;
     }
-public void recibir(){
 
-}
+    public void recibir() {
+
+    }
 
 public void recibirDanio(danio: int){
 
 }
 
-public boolean estaVivo(){
-    return false;
-    
-}
+    public boolean estaVivo() {
+        return false;
+
+    }
 
 }
-
-
-
