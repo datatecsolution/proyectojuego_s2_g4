b0ab06f5410059a6ff66dda7f62170c1606c0e1a
Rut Cruz <rutcruzavila763@gmail.com>
2025-08-11 09:18:50 -0600

    Agregue metodos y funciones a clase Jugador

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -1,27 +1,28 @@
 package net.perlasjp.juego;
 
 public class Jugador {
-private String nombre;
+    private String nombre;
     private int salud;
     private int fuerza;
 
-    public int atacar(){
-       return 0;
+    public int atacar() {
+        return fuerza;
     }
-public void recibir(){
+
+    public void recibir() {
+
+    }
+
+    public void recibirDanio(int cantidad) {
+        salud = salud - cantidad;
+        if (salud < 0)
+            salud = 0;
+
+    }
+
+    public boolean estaVivo() {
+        return salud > 0;
+
+    }
 
 }
-
-public void recibirDanio(danio: int){
-
-}
-
-public boolean estaVivo(){
-    return false;
-    
-}
-
-}
-
-
-
